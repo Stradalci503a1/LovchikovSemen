a6e9c3fd3411ab1f9de4d98f806fe6397b0634c0
Postavte5please <gopnika58@gmail.com>
2017-11-30 09:18:33 +0300

    Revert "summ"
    This reverts commit b16298b55d17fb79ed517652ea0862a8e70eaee2.

--- src/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-        String result = com.company.Formatter.build("Hello {0}, I am {1} years old", "hell", 15);
-        System.out.println(result);
-    }
-}
