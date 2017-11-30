7a451ee4abcf1ff03fc4eaddafcf24998ba32be7
Postavte5please <gopnika58@gmail.com>
2017-11-30 09:27:40 +0300

    puk
    daw

---
+++ lab06/src/PolarPoint.java
@@ -0,0 +1,22 @@
+public class PolarPoint {
+
+    private double rho;
+    private double phi;
+
+    PolarPoint(double rho, double phi){
+        this.rho = rho;
+        this.phi = phi;
+    }
+
+    public double getRho() {
+        return rho;
+    }
+
+    public double getPhi() {
+        return phi;
+    }
+
+    Point toCartesian(){
+        return new Point(rho * Math.cos(phi), rho * Math.sin(phi));
+    }
+}
